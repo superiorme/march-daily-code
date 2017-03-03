d0150e0093e18a018dd0e134abdb3bba9b311e5f
superiorme <634206@dpsk12.net>
2017-03-03 09:10:11 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<windows.h>
+
+using namespace std;
+void jukebox(int input);
+
+
+
+int main()
+{
+	void jukebox(int input);
+	int input;
+	int jukebox;
+
+	cout<<"what song do you what to listen 1,2,3,4,"<<endl;
+	cin >> input;
+
+	jukebox(input);
+
+	return 0;
+}
+void jukebox(int input) {
+	switch (input) {
+
+	case 1: 
+
+		playsound(text()
+	}
+	PlaySound("C:\\Users\\Commander\\Desktop\\test.wav", NULL, SND_ASYNC);
+}
